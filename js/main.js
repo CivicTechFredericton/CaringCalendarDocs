865b6240f46d4341f3b14cad61998e91ea3f57fe
Christine Harvie <christine.harvie@lookbooks.com>
2018-05-03 09:07:29 -0300

    initial setup of fullcalendar

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'month,agendaWeek,agendaDay',
+            center: 'title',
+            right:  'today prev,next'
+        },
+        themeSystem: 'bootstrap4'
+    });
+
+});
