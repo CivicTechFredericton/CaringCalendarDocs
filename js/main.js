886d5833b34a914d1ccf1826117f2cb00ba87fba
Christine Harvie <christine.harvie@lookbooks.com>
2018-05-15 19:49:58 -0300

    form validation

--- js/main.js
+++ js/main.js
@@ -9,4 +9,27 @@ $(function() {
         themeSystem: 'bootstrap4'
     });
 
+    $('#registration-form').on('submit', function(e) {
+        e.preventDefault();
+        var validated = true;
+        $('.validate', this).each(function() {
+            if ($(this).val() == '') {
+                validated = false;
+                $(this).addClass('invalid');
+            }
+        });
+        if ($('#password').val() != $('#password2').val()) {
+            $('#password2').addClass('invalid');
+            validated = false;
+        } else {
+            $('#password2').removeClass('invalid');
+        }
+        if (validated) {
+            // submit that form!
+        }
+        return false;
+    });
+    $('.validate').on('keyup', function() {
+        $(this).removeClass('invalid');
+    });
 });
